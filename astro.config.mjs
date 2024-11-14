1c918a3bd8d32e414855933c37a65eb9862bf97f
Simon Münker <muenker@uni-trier.de>
2024-11-14 15:10:09 +0100

    pre-alpha dummy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwind from '@astrojs/tailwind';
+import tailwind from "@astrojs/tailwind";
 
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), svelte()],
+  integrations: [tailwind(), svelte(), icon()],
   site: "https://simon-muenker.github.io",
   base: "TWONy-micro",
 });
--- prettier.config.mjs
+++ prettier.config.mjs
@@ -1,22 +1,22 @@
 /** @type {import("prettier").Config} */
 export default {
-    plugins: [
-      "prettier-plugin-astro",
-      "prettier-plugin-svelte",
-      "prettier-plugin-tailwindcss",
-    ],
-    overrides: [
-      {
-        files: "*.astro",
-        options: {
-          parser: "astro",
-        },
+  plugins: [
+    "prettier-plugin-astro",
+    "prettier-plugin-svelte",
+    "prettier-plugin-tailwindcss",
+  ],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
       },
-      {
-        files: "*.svelte",
-        options: {
-          parser: "svelte",
-        },
+    },
+    {
+      files: "*.svelte",
+      options: {
+        parser: "svelte",
       },
-    ],
-  };
+    },
+  ],
+};
