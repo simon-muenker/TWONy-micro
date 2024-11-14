e789d741298e60c9a2629ea30507382902298457
Simon Münker <simon.muenker@googlemail.com>
2024-11-14 09:46:35 +0100

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import tailwind from '@astrojs/tailwind';
+
+import svelte from '@astrojs/svelte';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), svelte()],
+  site: "https://simon-muenker.github.io",
+  base: "_astro_starter",
+});
