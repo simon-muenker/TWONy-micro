65ef9d7c473c5f28956a36eb2bec8e48199458ac
Simon Münker <muenker@uni-trier.de>
2024-12-02 11:08:22 +0100

    convert config to persistent store

--- src/stores/config.ts
+++ src/stores/config.ts
@@ -1,4 +1,4 @@
-import { deepMap } from "nanostores";
+import { persistentMap } from "@nanostores/persistent";
 
 export type Config = {
   agents: {
@@ -7,19 +7,28 @@ export type Config = {
     replyProp: number;
   };
   simulation: {
+    ranking: "chronological" | "emotion-based";
     tickTime: number;
     maxThreads: number;
   };
 };
 
-export const config = deepMap<Config>({
-  agents: {
-    model: "llama3.1:8b-instruct-q6_K",
-    postProp: 0.3,
-    replyProp: 0.7,
+export const config = persistentMap<Config>(
+  "config:",
+  {
+    agents: {
+      model: "llama3.1:8b-instruct-q6_K",
+      postProp: 0.3,
+      replyProp: 0.7,
+    },
+    simulation: {
+      ranking: "emotion-based",
+      tickTime: 4000,
+      maxThreads: 20,
+    },
   },
-  simulation: {
-    tickTime: 4000,
-    maxThreads: 20,
+  {
+    encode: JSON.stringify,
+    decode: JSON.parse,
   },
-});
+);
