8068277426c9f647423d94621edcef7ce52cdbad
Simon Münker <muenker@uni-trier.de>
2024-12-11 15:26:15 +0100

    make instruction and persona editable

--- src/components/Configure/AgentInstructions.svelte
+++ src/components/Configure/AgentInstructions.svelte
@@ -3,8 +3,16 @@
 </script>
 
 {#each Object.entries($instructionsStore) as [key, instruction], index (index)}
-  <h3 class="text-bold mb-1 text-xl">task: {key}</h3>
-  <p class="mb-2 text-sm text-gray-900" contenteditable>
-    {instruction.content}
-  </p>
+  <h3 class="text-bold mb-1 text-xl">{key}</h3>
+  <textarea
+    name={key}
+    id={key}
+    class="w-full grow resize-none rounded-lg border-0 bg-slate-100 bg-transparent p-2 text-sm text-slate-700 focus:outline-0"
+    value={instruction.content}
+    on:change={(newValue) =>
+      instructionsStore.setKey(key, {
+        ...instruction,
+        content: newValue.target.value,
+      })}
+  ></textarea>
 {/each}
--- src/components/Configure/AgentPersonas.svelte
+++ src/components/Configure/AgentPersonas.svelte
@@ -3,12 +3,21 @@
 </script>
 
 <div class="divide-y">
-  {#each Object.entries($personaAgentsStore) as [_, agent], index (index)}
+  {#each Object.entries($personaAgentsStore) as [key, agent], index (index)}
     <div class="py-8">
       <h3 class="text-bold mb-1 text-xl">{agent.icon} {agent.name}</h3>
-      <p class="mb-2 text-sm text-gray-900" contenteditable>
-        {agent.instruction}
-      </p>
+      <textarea
+        name={key}
+        id={key}
+        class="w-full grow resize-none rounded-lg border-0 bg-slate-100 bg-transparent p-2 text-sm text-slate-700 focus:outline-0"
+        value={agent.instruction}
+        rows="20"
+        on:change={(newValue) =>
+          personaAgentsStore.setKey(key, {
+            ...agent,
+            instruction: newValue.target.value,
+          })}
+      ></textarea>
     </div>
   {/each}
 </div>
