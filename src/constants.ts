77b3f4fbe56be076974e463741a4c14a91062531
Simon Münker <muenker@uni-trier.de>
2025-03-15 13:01:17 +0100

    update model selection

--- src/constants.ts
+++ src/constants.ts
@@ -17,10 +17,11 @@ export const PAGES: Array<{ icon: string; text: string; route: string }> = [
 ];
 
 export const MODELS: Array<string> = [
-  "llama3.1:8b-instruct-q6_K",
-  "llama3.3:70b-instruct-q6_K",
-  "mistral:7b-instruct-v0.2-q6_K",
-  "mixtral:8x7b-instruct-v0.1-q6_K",
+  "llama3.1:8b",
+  "llama3.3:70b",
+  "mistral:7b",
+  "mistral-large:123b",
+  "deepseek-r1:7b",
   "deepseek-r1:70b"
 ] as const;
 
